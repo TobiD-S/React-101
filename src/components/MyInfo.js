b48ad5c9001c686a810e919c9cb74593c009ecd9
Tobi <delesafatobi@gmail.com>
2020-05-09 22:17:38 +0100

    Create MyInfo.js

---
+++ src/components/MyInfo.js
@@ -0,0 +1,19 @@
+import React from "react"
+import NameTobi from "./NameTobi"
+
+function MyInfo() {
+    return ( 
+    <div style={{backgroundColor: "lightblue"}}> 
+        <h1 style={{color: 'red'}} >Tobi</h1> 
+        <p> I'm 23, I'm scared for my future, but I'm taking it a day at a time..I also love my family and Gbolahan. God is the best! </p> 
+        <ol>
+            <li> Greece </li> 
+            <li> Cayman Islands </li> 
+            <li> Turks and Caicos </li> 
+        </ol> 
+        <NameTobi/> 
+    </div> )
+
+}
+
+    export default MyInfo
