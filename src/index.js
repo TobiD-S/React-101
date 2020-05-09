9d0556d93880c9ac1cc28599e0e8ca586551fd0d
Tobi <delesafatobi@gmail.com>
2020-05-09 11:52:36 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render( <
     h1 >
-    Hello Tobi!
+    Hi Tobi!
     <
     /h1>,
     document.getElementById('root')
@@ -15,4 +15,4 @@ ReactDOM.render( <
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+serviceWorker.register();
