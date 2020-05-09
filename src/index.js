849d844142c7b92346aa31ef58a54ee8a7c1fad6
Tobi <delesafatobi@gmail.com>
2020-05-09 22:17:53 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,13 +3,11 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import MyInfo from "./components/MyInfo"
+import NameTobi from "./components/NameTobi"
 
-ReactDOM.render( <
-    h1 >
-    Hi Tobi!
-    <
-    /h1>,
-    document.getElementById('root')
+ReactDOM.render( <MyInfo/> ,
+    document.getElementById("root")
 );
 
 // If you want your app to work offline and load faster, you can change
