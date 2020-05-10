03905d48014d62d929ed7c1148c3cedcc8312244
Tobi <delesafatobi@gmail.com>
2020-05-10 02:06:19 +0100

    Update MyInfo.js

--- src/components/MyInfo.js
+++ src/components/MyInfo.js
@@ -1,9 +1,12 @@
 import React from "react"
 import NameTobi from "./NameTobi"
+import NavBar from "./NavBar"
+import Footer from "./Footer"
 
 function MyInfo() {
     return ( 
     <div style={{backgroundColor: "lightblue"}}> 
+    <NavBar/>
         <h1 style={{color: 'red'}} >Tobi</h1> 
         <p> I'm 23, I'm scared for my future, but I'm taking it a day at a time..I also love my family and Gbolahan. God is the best! </p> 
         <ol>
@@ -11,7 +14,8 @@ function MyInfo() {
             <li> Cayman Islands </li> 
             <li> Turks and Caicos </li> 
         </ol> 
-        <NameTobi/> 
+        <NameTobi/>
+        <Footer/> 
     </div> )
 
 }
