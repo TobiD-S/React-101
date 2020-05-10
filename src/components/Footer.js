d20f2d842a1894362c25c2a3d3b31aca527d0501
Tobi <delesafatobi@gmail.com>
2020-05-10 02:06:13 +0100

    Create Footer.js

---
+++ src/components/Footer.js
@@ -0,0 +1,14 @@
+import React from "react"
+
+function Footer(){
+    return(
+        <footer>
+            <ul>
+                <li>Socials</li>
+                <li>Contact Us!</li>
+            </ul>
+        </footer>
+    )
+}
+
+export default Footer
