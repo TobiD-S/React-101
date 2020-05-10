b7918ee70ed5ef340c785170a7b6f5fbcc21e94a
Tobi <delesafatobi@gmail.com>
2020-05-10 02:06:25 +0100

    Create NavBar.js

---
+++ src/components/NavBar.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function NavBar(){
+    return (
+        <nav>
+            <ul>
+                <li>Home</li>
+                <li>About Us</li>
+                <li>Shop</li>
+            </ul>
+        </nav>
+    )
+}
+
+export default NavBar
